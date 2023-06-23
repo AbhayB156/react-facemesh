fd2dc1f88c931822edb595af0875e4e615a71c94
AbhayB156 <abhaybhosle70@gmail.com>
2023-06-23 22:07:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,79 @@
+import React, { useRef } from 'react';
+import * as tf from "@tensorflow/tfjs";
+import * as facemesh from "@tensorflow-models/facemesh";
+import Webcam from "react-webcam";
+import { drawMesh } from "./utilities";
+
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+
+const webcamRef = useRef(null);
+const canvasRef = useRef(null);
+
+const runfacemesh = async () => {
+  const net = await facemesh.load({
+    inputResolution:{width:640, height:480}, scal: 0.8,
+  });
+  setInterval(()=>{
+    detect(net)
+  }, 100)
+};
+
+const detect = async(net) => {
+  if(typeof webcamRef.current !=="undefined" && webcamRef.current !== null && webcamRef.current.video.readyState === 4)
+  {
+    const video = webcamRef.current.video;
+    const videoWidth = webcamRef.current.video.videoWidth;
+    const videoHeight = webcamRef.current.video.videoHeight;
+
+    webcamRef.current.video.width = videoWidth;
+    webcamRef.current.video.Height = videoHeight;
+
+    canvasRef.current.width = videoWidth;
+    canvasRef.current.height = videoHeight;
+
+    const face = await net.estimateFaces(video);
+    console.log(face);
+
+    const ctx = canvasRef.current.getContext("2d");
+    drawMesh(face, ctx)
+  }
+};
+runfacemesh();
+
+  return (
+    <div className="App">
+      <header className="/App-header">
+      <Webcam ref = {webcamRef} style ={
+        {
+          position:"absolute",
+          marginLeft:"auto",
+          left:0,
+          right:0,
+          textAlign:"center",
+          zIndex:9,
+          width:648,
+          height:480
+        }
+      }/>
+      <canvas ref={ canvasRef } style ={
+      {
+        position:"absolute",
+        marginLeft:"auto",
+        left:0,
+        right:0,
+        textAlign:"center",
+        zIndex:9,
+        width:648,
+         height:480
+      }
+    }
+      />
+      </header>
+    </div>
+  );
+}
+
+export default App;
